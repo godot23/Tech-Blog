f09393f919e88b9bed34cf7b6d3459b22ce8db51
godot23 <jasonanavon@gmail.com>
2023-09-04 18:41:05 -0400

    added more routes to home routes and user routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const {User, Post} = require("../models")
+const { User, Post } = require("../models")
+const withAuth = require("../utis/auth");
 
-router.get('/', async (req, res) =>{
-    try{
+router.get('/', async (req, res) => {
+    try {
         const getPosts = await Post.findAll({
             include: [
                 {
@@ -12,15 +13,44 @@ router.get('/', async (req, res) =>{
             ],
         });
 
-        const posts = getPosts.map((post) => post.get({plain: true}));
+        const posts = getPosts.map((post) => post.get({ plain: true }));
         res.render('homepage', {
             posts,
             logged_in: req.session.logged_in
         });
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err);
     }
 });
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{
+                model: User,
+                attributes: ['user_name'],
+            },
+            ],
+        })
+        res.render('dashboard', {
+            ...posts,
+            logged_in: true
+        });
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router;
